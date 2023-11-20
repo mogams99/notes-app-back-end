d2c7d326193e3167089faa2bb101b48097947b27
Mochammad Gamal <mogams1811@gmail.com>
2023-11-20 13:37:50 +0700

    feat: :sparkles: implementasi modul
    modul yang di implementasikan antarai lain: antara lain: menampilkan catatan, mengubah catatan, dan mengapus catatan.

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,13 @@
 const notes = require("./notes");
-const { nanoid } = require("nanoid");
+const {
+  nanoid
+} = require("nanoid");
+
+// ? fungsi untuk menampilkan halaman home
+const showPageHome = () => ({
+  status: "success",
+  message: "Welcome to Notes App API!",
+});
 
 // ? fungsi untuk get all notes
 const getAllNotesHandler = () => ({
@@ -11,7 +19,11 @@ const getAllNotesHandler = () => ({
 
 // ? fungsi untuk tambah notes
 const addNoteHandler = (request, h) => {
-  const { title, tags, body } = request.payload;
+  const {
+    title,
+    tags,
+    body
+  } = request.payload;
 
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
@@ -50,7 +62,99 @@ const addNoteHandler = (request, h) => {
   return response;
 };
 
+// ? fungi untuk get notes by id
+const getNoteByIdHandler = (request, h) => {
+  const {
+    id
+  } = request.params;
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: "success",
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Catatan tidak ditemukan!",
+  });
+  response.code(404);
+  return response;
+};
+
+// ? fungsi untuk edit notes
+const editNoteByIdHandler = (request, h) => {
+  const {
+    id
+  } = request.params;
+  const {
+    title,
+    tags,
+    body
+  } = request.payload;
+
+  const updatedAt = new Date().toISOString();
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: "success",
+      message: "Catatan berhasil diperbarui!",
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: "fail",
+    message: "Catatan gagal berhasil diperbarui!",
+  });
+  response.code(404);
+  return response;
+};
+
+// ? fungsi untuk delete notes
+const deleteNoteByIdHandler = (request, h) => {
+  const {
+    id
+  } = request.params;
+  const index = notes.findIndex((note) => note.id === id);
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus!'
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan!',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
+  showPageHome,
   addNoteHandler,
   getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
 };
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
-const { addNoteHandler, getAllNotesHandler } = require("./handler");
+const { showPageHome, addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require("./handler");
 
 const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: showPageHome,
+  },
   {
     method: "POST",
     path: "/notes",
@@ -10,6 +15,21 @@ const routes = [
     method: "GET",
     path: "/notes",
     handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
   },
 ];
 
